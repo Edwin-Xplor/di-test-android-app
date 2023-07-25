7f02f37276cdd9bfb5712055e6c73d3b69169ab6
Edwin Murari <edwin.murari+xplorid@myxplor.com>
2023-07-25 16:22:40 +1000

    Updated gradle and libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
-    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
 }
 buildscript {
